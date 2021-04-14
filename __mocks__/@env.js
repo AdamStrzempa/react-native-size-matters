588d7399a0ca7900079e3663163ee71040c03b79
AdamStrzempa <adams@reactpoland.com>
2021-04-14 22:11:12 +0200

    fix: update default base dimensions

--- __mocks__/@env.js
+++ __mocks__/@env.js
@@ -1,6 +1,6 @@
 const dotenv = {
-    SIZE_MATTERS_BASE_WIDTH: 525,
-    SIZE_MATTERS_BASE_HEIGHT: 1020
+    SIZE_MATTERS_BASE_WIDTH: 375,
+    SIZE_MATTERS_BASE_HEIGHT: 812
 };
 
 module.exports = dotenv;
